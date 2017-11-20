673e09aa5889087d44bb60a9b5ace7d378c49290
Stephany Choi <s.choi417@gmail.com>
2017-11-20 13:28:34 -0500

    Create canvas whiteboard

---
+++ whiteboard.js
@@ -0,0 +1,76 @@
+window.onload = function() {
+  var canvas = document.getElementById('myCanvas');
+  var context = canvas.getContext("2d");
+
+  context.fillStyle = "#fff";
+  context.fillRect(0,0, canvas.width, canvas.height);
+
+  //Mouse Event Handlers
+  if(canvas){
+    var mouseDown = false;
+    var canvasX;
+    var canvasY;
+    context.linewidth = 3;
+
+    $(canvas).mousedown(function(e){
+      mouseDown = true;
+      context.beginPath();
+      canvasX = e.pageX - canvas.offsetLeft;
+      canvasY = e.pageY - canvas.offsetTop;
+      context.lineTo(canvasX, canvasY);
+      context.strokeStyle = "#000";
+      context.stroke();
+    })
+    .mousemove(function(e){
+      if(mouseDown !== false) {
+        canvasX = e.pageX - canvas.offsetLeft;
+        canvasY = e.pageY - canvas.offsetTop;
+        context.lineTo(canvasX, canvasY);
+        context.strokeStyle = "#000";
+        context.stroke();
+      }
+    })
+    .mouseup(function(e){
+      mouseDown = false;
+      context.closePath();
+    });
+  }
+
+// Touch event functions
+  var draw = {
+    started: false,
+
+    start: function(event) {
+      context.beginPath();
+      context.moveTo(
+        event.touches[0].pageX,
+        event.touches[0].pageY
+      );
+      this.started = true;
+    },
+
+    move: function(event) {
+      context.lineTo(
+        event.touches[0].pageX,
+        event.touches[0].pageY
+      );
+
+      context.strokeStyle = "#000";
+      context.lineWidth = 3;
+      context.stroke();
+    },
+
+    end: function(event){
+      this.started = false;
+    }
+  };
+//Touch event listeners
+  canvas.addEventListener('touchstart', draw.start, false);
+  canvas.addEventListener('touchend', draw.end, false);
+  canvas.addEventListener('touchmove', draw.move, false);
+
+//Disable Page Move
+  document.body.addEventListener('touchmove', function(event){
+    event.preventDefault();
+  }, false);
+};
